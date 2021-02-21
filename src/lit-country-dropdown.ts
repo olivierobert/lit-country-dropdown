7d3bd6b9058303bbfbbddba0c4d7ab8e76b8962d
Olivier Robert <olivierobert@gmail.com>
2021-02-21 14:37:33 +0700

    Implement the option popover (#3)

--- src/lit-country-dropdown.ts
+++ src/lit-country-dropdown.ts
@@ -10,37 +10,164 @@ import {LitElement, html, customElement, property, css} from 'lit-element';
 export class LitCountryDropdown extends LitElement {
   static styles = css`
     :host {
-      display: block;
+      --accent-color: rgb(79, 70, 229);
+
+      display: inline-block;
+      min-width: 300px;
+
+      font-size: 0.875rem;
+      font-family: sans-serif;
+
+      /* TODO: Remove boilerplate styles */
       border: solid 1px gray;
       padding: 16px;
       max-width: 800px;
     }
+
+    :host * {
+      box-sizing: border-box;
+    }
+
+    .country-dropdown {
+      position: relative;
+    }
+
+    .country-dropdown__popover--hidden {
+      display: none;
+    }
+
+    .country-dropdown__button,
+    .country-dropdown__popover {
+      width: 100%;
+
+      background: rgb(255, 255, 255);
+      border: 1px solid rgb(204, 204, 204);
+      border-radius: 6px;
+    }
+
+    .country-dropdown__option {
+      padding: 8px 12px;
+
+      font-size: inherit;
+    }
+
+    .country-dropdown__option:first-child {
+      border-top-right-radius: 6px;
+      border-top-left-radius: 6px;
+    }
+
+    .country-dropdown__option:last-child {
+      border-bottom-right-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    .country-dropdown__button {
+      display: inline-block;
+      padding: 0;
+
+      text-align: left;
+    }
+
+    .country-dropdown__button:focus {
+      border-color: var(--accent-color);
+      box-shadow: 1px 1px 1px var(--accent-color), -1px -1px 1px var(--accent-color), 0 0 #0000;
+      outline: transparent;
+    }
+
+    .country-dropdown__option {
+      display: flex;
+      align-items: center;
+
+      cursor: pointer;
+    }
+    .country-dropdown__option > * {
+      height: 20px;
+      line-height: 20px;
+    }
+
+    .country-dropdown__name {
+      flex-grow: 1;
+      margin-left: 12px;
+    }
+
+    .country-dropdown__icon {
+      width: 20px;
+      margin-left: 12px;
+    }    
+
+    .country-dropdown__list {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+
+    .country-dropdown__popover {
+      position: absolute;
+      margin-top: 6px;
+    }
+
+    .country-dropdown__popover .country-dropdown__option:hover {
+      color: rgb(255, 255, 255);
+
+      background-color: var(--accent-color);
+    }
   `;
 
-  /**
-   * The name to say "Hello" to.
-   */
-  @property()
-  name = 'World';
+  @property({type: String})
+  name = 'country';
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({type: Number})
-  count = 0;
+  @property({type: Boolean})
+  showPopover = false;
 
   render() {
     return html`
-      <h1>Hello, ${this.name}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
+      <div class="country-dropdown">
+        <select id="${this.name}" name="${this.name}" hidden>
+          <option value="fr">France</option>
+          <option value="th">Thailand</option>
+        </select>
+
+        <button type="button" class="country-dropdown__button" @click="${this._togglePopover}">
+          <div class="country-dropdown__option">
+            <div class="country-dropdown__flag">🇫🇷</div>
+            <div class="country-dropdown__name">France</div>
+            <div class="country-dropdown__icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </div>
+          </div>
+        </button>
+
+        <div class="country-dropdown__popover ${!this.showPopover ? "country-dropdown__popover--hidden" : "" }">
+          <ul class="country-dropdown__list">
+            <li class="country-dropdown__option" data-value="fr">
+              <div class="country-dropdown__flag">🇫🇷</div>
+              <div class="country-dropdown__name">France</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+            <li class="country-dropdown__option" data-value="th">
+              <div class="country-dropdown__flag">🇹🇭</div>
+              <div class="country-dropdown__name">Thailand</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <slot></slot>
+      </div>
     `;
   }
 
-  private _onClick() {
-    this.count++;
+  private _togglePopover() {
+    this.showPopover = !this.showPopover;
   }
 
   foo(): string {
--- src/test/lit-country-dropdown_test.ts
+++ src/test/lit-country-dropdown_test.ts
@@ -14,36 +14,156 @@ suite('lit-country-dropdown', () => {
     assert.shadowDom.equal(
       el,
       `
-      <h1>Hello, World!</h1>
-      <button part="button">Click Count: 0</button>
-      <slot></slot>
+      <div class="country-dropdown">
+        <select id="country" name="country" hidden>
+          <option value="fr">France</option>
+          <option value="th">Thailand</option>
+        </select>
+
+        <button type="button" class="country-dropdown__button">
+          <div class="country-dropdown__option">
+            <div class="country-dropdown__flag">🇫🇷</div>
+            <div class="country-dropdown__name">France</div>
+            <div class="country-dropdown__icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </div>
+          </div>
+        </button>
+
+        <div class="country-dropdown__popover country-dropdown__popover--hidden">
+          <ul class="country-dropdown__list">
+            <li class="country-dropdown__option" data-value="fr">
+              <div class="country-dropdown__flag">🇫🇷</div>
+              <div class="country-dropdown__name">France</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+            <li class="country-dropdown__option" data-value="th">
+              <div class="country-dropdown__flag">🇹🇭</div>
+              <div class="country-dropdown__name">Thailand</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <slot></slot>
+      </div>
     `
     );
   });
 
   test('renders with a set name', async () => {
-    const el = await fixture(html`<lit-country-dropdown name="Test"></lit-country-dropdown>`);
+    const el = await fixture(html`<lit-country-dropdown name="location"></lit-country-dropdown>`);
+    
     assert.shadowDom.equal(
       el,
       `
-      <h1>Hello, Test!</h1>
-      <button part="button">Click Count: 0</button>
-      <slot></slot>
+      <div class="country-dropdown">
+        <select id="location" name="location" hidden>
+          <option value="fr">France</option>
+          <option value="th">Thailand</option>
+        </select>
+
+        <button type="button" class="country-dropdown__button">
+          <div class="country-dropdown__option">
+            <div class="country-dropdown__flag">🇫🇷</div>
+            <div class="country-dropdown__name">France</div>
+            <div class="country-dropdown__icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </div>
+          </div>
+        </button>
+
+        <div class="country-dropdown__popover country-dropdown__popover--hidden">
+          <ul class="country-dropdown__list">
+            <li class="country-dropdown__option" data-value="fr">
+              <div class="country-dropdown__flag">🇫🇷</div>
+              <div class="country-dropdown__name">France</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+            <li class="country-dropdown__option" data-value="th">
+              <div class="country-dropdown__flag">🇹🇭</div>
+              <div class="country-dropdown__name">Thailand</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <slot></slot>
+      </div>
     `
     );
   });
 
-  test('handles a click', async () => {
+  test('opens the popover', async () => {
     const el = (await fixture(html`<lit-country-dropdown></lit-country-dropdown>`)) as LitCountryDropdown;
     const button = el.shadowRoot!.querySelector('button')!;
+    
     button.click();
     await el.updateComplete;
+
     assert.shadowDom.equal(
       el,
       `
-      <h1>Hello, World!</h1>
-      <button part="button">Click Count: 1</button>
-      <slot></slot>
+      <div class="country-dropdown">
+        <select id="country" name="country" hidden>
+          <option value="fr">France</option>
+          <option value="th">Thailand</option>
+        </select>
+
+        <button type="button" class="country-dropdown__button">
+          <div class="country-dropdown__option">
+            <div class="country-dropdown__flag">🇫🇷</div>
+            <div class="country-dropdown__name">France</div>
+            <div class="country-dropdown__icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </div>
+          </div>
+        </button>
+
+        <div class="country-dropdown__popover">
+          <ul class="country-dropdown__list">
+            <li class="country-dropdown__option" data-value="fr">
+              <div class="country-dropdown__flag">🇫🇷</div>
+              <div class="country-dropdown__name">France</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+            <li class="country-dropdown__option" data-value="th">
+              <div class="country-dropdown__flag">🇹🇭</div>
+              <div class="country-dropdown__name">Thailand</div>
+              <div class="country-dropdown__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <slot></slot>
+      </div>
     `
     );
   });
